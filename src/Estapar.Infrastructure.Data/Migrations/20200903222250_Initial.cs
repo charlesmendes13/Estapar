adbf795f4fd3ebf2fb98f44ded84ec53691b9999
charlesmendes13 <charlesluizmendes@gmail.com>
2020-09-03 19:28:37 -0300

    Adionando LocalDB

---
+++ src/Estapar.Infrastructure.Data/Migrations/20200903222250_Initial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estapar.Infrastructure.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(nullable: false),
+                    Modelo = table.Column<string>(nullable: false),
+                    Placa = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carro", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manobrista",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: false),
+                    CPF = table.Column<string>(nullable: false),
+                    Data_Nascimento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manobrista", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarroManobrista",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Carro = table.Column<int>(nullable: false),
+                    Manobrista = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarroManobrista", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarroManobrista_Carro_Carro",
+                        column: x => x.Carro,
+                        principalTable: "Carro",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarroManobrista_Manobrista_Manobrista",
+                        column: x => x.Manobrista,
+                        principalTable: "Manobrista",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarroManobrista_Carro",
+                table: "CarroManobrista",
+                column: "Carro");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarroManobrista_Manobrista",
+                table: "CarroManobrista",
+                column: "Manobrista");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarroManobrista");
+
+            migrationBuilder.DropTable(
+                name: "Carro");
+
+            migrationBuilder.DropTable(
+                name: "Manobrista");
+        }
+    }
+}
--- src/Estapar.Presentation.Web/Startup.cs
+++ src/Estapar.Presentation.Web/Startup.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Estapar.Application;
+using Estapar.Infrastructure.Data;
+using Estapar.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,15 +19,36 @@ namespace Estapar.Presentation.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostEnvironment env)
         {
             Configuration = configuration;
+
+            //
+
+            string baseDir = env.ContentRootPath;
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(baseDir, "App_Data"));
+
+            var feedDirectoryPath = $"{AppDomain.CurrentDomain.GetData("DataDirectory")}";
         }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Context
+
+            services.AddDbContextPool<Context>(option =>
+                 option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+             );
+
+            // IoC
+
+            InjectorDependency.Register(services);
+
+            // AutoMapper
+
+            services.AddAutoMapper(x => x.AddProfile(new MappingEntities()));
+
             services.AddControllersWithViews();
         }
 
